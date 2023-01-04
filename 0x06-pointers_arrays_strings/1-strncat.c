fa632eca116f550ba589cf05e88a149efdd27158
AdmireTM <admiremuhenyeri@gmail.com>
2023-01-04 15:43:29 -0800

    dee

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  *  *_strcat - concatenates  the stringst
@@ -6,18 +6,18 @@ nclude "main.h"
  *     *@src: String to be concatenated upon
  *      *
  *       * Return: returns poiner to @dest
- *        */
+ */
 
 char *_strcat(char *dest, char *src)
 {
-
 		int index = 0, dest_len = 0;
 
 			while (dest[index++])
-						dest_len++;
+				dest_len++;
 
-				for (index = 0; src[index]; index++)
-							dest[dest_len++] = src[index];
+				for (index = 0; src[index];
+						index++)
+					dest[dest_len++] = src[index];
 
 					return (dest);
 }
